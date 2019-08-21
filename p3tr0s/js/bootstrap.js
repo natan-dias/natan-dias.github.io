2f5c7587f3f20c93c8d37ddcd40e446c97cc7dd4
natandino <natandino@len-hd.localdomain>
2019-08-21 19:56:41 -0300

    petros21082019

--- p3tr0s/js/bootstrap.js
+++ p3tr0s/js/bootstrap.js
@@ -943,3 +943,10 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
         })
     })
 }(jQuery);
+
+/*Dropdown Menu*/
+$('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
